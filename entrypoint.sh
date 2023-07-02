2020cb53c128a39162f28a0861ab7c2ae0540cce
Emmie Maeda <emmie.maeda@gmail.com>
2023-07-02 03:57:48 -0400

    Use /bin/sh as entrypoint shell.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-if [[ -z "$AWS_S3_BUCKET" ]]; then
+if [ -z "$AWS_S3_BUCKET" ]; then
 	echo 'AWS_S3_BUCKET is not set. Quitting.'
 	exit 1
 fi
 
-if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
 	echo 'AWS_ACCESS_KEY_ID is not set. Quitting.'
 	exit 1
 fi
 
-if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
 	echo 'AWS_SECRET_ACCESS_KEY is not set. Quitting.'
 	exit 1
 fi
 
-if [[ -z "$AWS_REGION" ]]; then
+if [ -z "$AWS_REGION" ]; then
 	echo 'AWS_REGION is not set. Quitting.'
 	exit 1
 fi
