8d64acfb367ee42ac19685d37985c4c55f863f29
Emmie Maeda <emmie.maeda@gmail.com>
2023-07-02 00:41:47 -0400

    Update entrypoint.sh script and documentation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,48 +1,43 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
-  exit 1
+if [[ -z "$AWS_S3_BUCKET" ]]; then
+	echo 'AWS_S3_BUCKET is not set. Quitting.'
+	exit 1
 fi
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+	echo 'AWS_ACCESS_KEY_ID is not set. Quitting.'
+	exit 1
 fi
 
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+	echo 'AWS_SECRET_ACCESS_KEY is not set. Quitting.'
+	exit 1
 fi
 
-# Default to us-east-1 if AWS_REGION not set.
-if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
-fi
-
-# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
-if [ -n "$AWS_S3_ENDPOINT" ]; then
-  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
+if [[ -z "$AWS_REGION" ]]; then
+	echo 'AWS_REGION is not set. Quitting.'
+	exit 1
 fi
 
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
 aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
+$AWS_ACCESS_KEY_ID
+$AWS_SECRET_ACCESS_KEY
+$AWS_REGION
 text
 EOF
 
-# Sync using our dedicated profile and suppress verbose messages.
-# All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
-              --no-progress \
-              ${ENDPOINT_APPEND} $*"
+# Run our custom S3 sync script
+python -m s3_sync \
+	--source "$SOURCE_DIR" \
+	--dest "$DEST_DIR" \
+	--bucket "$AWS_S3_BUCKET" \
+	--profile s3-sync-action \
+	"$@"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
